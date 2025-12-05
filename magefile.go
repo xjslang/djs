588e204a82f3eefaf32aa21d73037139ab7b5efc
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-12-05 17:47:55 +0000

    add benchmark function to magefile

--- magefile.go
+++ magefile.go
@@ -6,6 +6,12 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
+// Bench runs project benchmarks using 'go test -bench=.'
+func Bench() error {
+	return sh.RunV("go", "test", "-bench=.")
+}
+
+// Lint runs linting
 func Lint() error {
 	return sh.RunV("golangci-lint", "run")
 }
