a0120aacea8e09158d41316f5ab3608dc89d6632
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-12-06 15:53:48 +0000

    update main.go

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/xjslang/djs/plugins"
 	"github.com/xjslang/xjs/lexer"
 	"github.com/xjslang/xjs/parser"
 )
+
+func execCommand(source string) error {
+	cmd := exec.Command("node", "-e", source)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Run()
+}
 
 func main() {
 	filename := os.Args[1]
@@ -27,5 +36,6 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Println(program.String())
+
+	execCommand(program.String())
 }
