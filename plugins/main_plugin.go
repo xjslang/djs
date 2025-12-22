e041a60723d01f0cf5efb95f53202fb4a0ed4a87
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-12-22 13:05:36 +0000

    refactor: rename IIFEFunctionDeclaration to MainFunctionDeclaration and update WriteTo method

--- plugins/main_plugin.go
+++ plugins/main_plugin.go
@@ -6,31 +6,17 @@ import (
 	"github.com/xjslang/xjs/token"
 )
 
-// IIFEFunctionDeclaration wraps a function declaration in an IIFE
-type IIFEFunctionDeclaration struct {
+type MainFunctionDeclaration struct {
 	*ast.FunctionDeclaration
 }
 
-// WriteTo generates an IIFE: (function name() {...})()
-func (iife *IIFEFunctionDeclaration) WriteTo(cw *ast.CodeWriter) {
-	cw.WriteString("(function ")
-	iife.Name.WriteTo(cw)
-	cw.WriteRune('(')
-	for i, param := range iife.Parameters {
-		if i > 0 {
-			cw.WriteRune(',')
-		}
-		param.WriteTo(cw)
-	}
-	cw.WriteRune(')')
-	iife.Body.WriteTo(cw)
-	cw.WriteString(")()")
+func (mfd *MainFunctionDeclaration) WriteTo(cw *ast.CodeWriter) {
+	mfd.FunctionDeclaration.WriteTo(cw)
+	cw.WriteString(";main()")
 }
 
-// MainPlugin transforms top-level 'main' functions into IIFEs
 func MainPlugin(pb *parser.Builder) {
 	pb.UseStatementInterceptor(func(p *parser.Parser, next func() ast.Statement) ast.Statement {
-		// Only intercept FUNCTION tokens
 		if p.CurrentToken.Type != token.FUNCTION {
 			return next()
 		}
@@ -42,15 +28,12 @@ func MainPlugin(pb *parser.Builder) {
 
 		// Parse the function normally
 		funcDecl := p.ParseFunctionStatement()
-
-		// Check if the function name is "main"
 		if funcDecl.Name != nil && funcDecl.Name.Value == "main" {
-			return &IIFEFunctionDeclaration{
+			return &MainFunctionDeclaration{
 				FunctionDeclaration: funcDecl,
 			}
 		}
 
-		// Return the original function if it's not named "main"
 		return funcDecl
 	})
 }
