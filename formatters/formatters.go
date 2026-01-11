c0c058ac065865987fe2eddcc8a5fcc04dee9a67
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-01-11 23:40:06 +0000

    refactor: enhance expression and statement preparation with nil checks and improved traversal

--- formatters/formatters.go
+++ formatters/formatters.go
@@ -80,22 +80,35 @@ func prepareStatements(stmts []ast.Statement) []ast.Statement {
 }
 
 func prepareExpression(exp ast.Expression) ast.Expression {
+	if exp == nil {
+		return nil
+	}
+
 	switch v := exp.(type) {
 	// wrap custom expressions with a "formatter" and return it
 	case *plugins.OrExpression:
+		v.Expression = prepareExpression(v.Expression)
 		return &OrExpressionFormatter{OrExpression: v}
 	// traverse the rest of the tree
 	case *plugins.DeferFunctionExpression:
 		v.Body.Statements = prepareStatements(v.Body.Statements)
+		return v
+	case *ast.FunctionExpression:
+		v.Body.Statements = prepareStatements(v.Body.Statements)
+		return v
 	case *ast.BinaryExpression:
 		v.Left = prepareExpression(v.Left)
 		v.Right = prepareExpression(v.Right)
+		return v
 	case *ast.UnaryExpression:
 		v.Right = prepareExpression(v.Right)
+		return v
 	case *ast.PostfixExpression:
 		v.Left = prepareExpression(v.Left)
+		return v
 	case *ast.GroupedExpression:
 		v.Expression = prepareExpression(v.Expression)
+		return v
 	case *ast.CallExpression:
 		v.Function = prepareExpression(v.Function)
 		args := []ast.Expression{}
@@ -103,21 +116,30 @@ func prepareExpression(exp ast.Expression) ast.Expression {
 			args = append(args, prepareExpression(arg))
 		}
 		v.Arguments = args
+		return v
 	case *ast.MemberExpression:
 		v.Object = prepareExpression(v.Object)
+		if !v.Computed {
+			// Property is an identifier, don't traverse
+			return v
+		}
 		v.Property = prepareExpression(v.Property)
+		return v
 	case *ast.AssignmentExpression:
 		v.Left = prepareExpression(v.Left)
 		v.Value = prepareExpression(v.Value)
+		return v
 	case *ast.CompoundAssignmentExpression:
 		v.Left = prepareExpression(v.Left)
 		v.Value = prepareExpression(v.Value)
+		return v
 	case *ast.ArrayLiteral:
 		elems := []ast.Expression{}
 		for _, elem := range v.Elements {
 			elems = append(elems, prepareExpression(elem))
 		}
 		v.Elements = elems
+		return v
 	case *ast.ObjectLiteral:
 		props := []ast.ObjectProperty{}
 		for _, prop := range v.Properties {
@@ -126,34 +148,58 @@ func prepareExpression(exp ast.Expression) ast.Expression {
 			props = append(props, prop)
 		}
 		v.Properties = props
+		return v
 	}
 	return exp
 }
 
 func prepareStatement(node ast.Statement) ast.Statement {
+	if node == nil {
+		return nil
+	}
+
 	switch v := node.(type) {
 	// wrap custom statements with a "formatter" and return it
 	case *plugins.DeferStatement:
+		v.Body.Statements = prepareStatements(v.Body.Statements)
 		return &DeferStatementFormatter{DeferStatement: v}
 	// traverse the rest of the tree
 	case *ast.Program:
 		v.Statements = prepareStatements(v.Statements)
+		return v
 	case *plugins.DeferFunctionDeclaration:
 		v.Body.Statements = prepareStatements(v.Body.Statements)
+		return v
+	case *ast.FunctionDeclaration:
+		v.Body.Statements = prepareStatements(v.Body.Statements)
+		return v
 	case *plugins.ExpressionStatement:
 		v.Expression = prepareExpression(v.Expression)
+		return v
 	case *plugins.LetStatement:
 		v.Value = prepareExpression(v.Value)
+		return v
 	case *ast.IfStatement:
 		v.Condition = prepareExpression(v.Condition)
+		v.ThenBranch = prepareStatement(v.ThenBranch)
+		v.ElseBranch = prepareStatement(v.ElseBranch)
+		return v
 	case *ast.WhileStatement:
 		v.Condition = prepareExpression(v.Condition)
+		v.Body = prepareStatement(v.Body)
+		return v
 	case *ast.ReturnStatement:
 		v.ReturnValue = prepareExpression(v.ReturnValue)
+		return v
 	case *ast.ForStatement:
 		v.Init = prepareExpression(v.Init)
 		v.Condition = prepareExpression(v.Condition)
 		v.Update = prepareExpression(v.Update)
+		v.Body = prepareStatement(v.Body)
+		return v
+	case *ast.BlockStatement:
+		v.Statements = prepareStatements(v.Statements)
+		return v
 	}
 	return node
 }
