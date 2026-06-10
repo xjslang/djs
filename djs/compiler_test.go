7a49bbe4f868ec14e8c89866c1536b9d94646424
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-06-10 23:32:53 +0100

    refactor(compiler): use contexts

---
+++ djs/compiler_test.go
@@ -0,0 +1,98 @@
+package djs_test
+
+import (
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/xjslang/djs/djs"
+)
+
+func TestCompiler(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name: "consecutive defers",
+			input: `{
+				defer console.log('a')
+				defer console.log('b')
+			}`,
+			expected: "b\na\n",
+		},
+		{
+			name: "separate blocks",
+			input: `
+			{ defer console.log('a') }
+			{ defer console.log('b') }`,
+			expected: "a\nb\n",
+		},
+		{
+			name: "nested blocks",
+			input: `
+			{
+				defer console.log('a')
+				{ defer console.log('b') }
+			}`,
+			expected: "b\na\n",
+		},
+		{
+			name: "mixed",
+			input: `
+			{
+				defer console.log('a')
+				{ defer console.log('b') }
+				defer console.log('c')
+			}
+			{ defer console.log('d') }`,
+			expected: "b\nc\na\nd\n",
+		},
+	}
+	requireNode16OrGreater(t)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := djs.Parse([]byte(test.input))
+			require.NoError(t, err)
+			jsCode := djs.Compile(result)
+			// execute JS code
+			cmd := exec.Command("node")
+			cmd.Stdin = strings.NewReader(jsCode)
+			output, err := cmd.Output()
+			require.NoError(t, err)
+			require.Equal(t, test.expected, string(output))
+		})
+	}
+}
+
+func TestCompiler_misusedDefer(t *testing.T) {
+	input := "defer console.log('aaa')"
+	result, err := djs.Parse([]byte(input))
+	require.NoError(t, err)
+	require.Panics(t, func() {
+		djs.Compile(result)
+	}, "should panic when using defer outside a block")
+}
+
+func requireNode16OrGreater(t *testing.T) {
+	t.Helper()
+	// test that node is installed
+	nodePath, err := exec.LookPath("node")
+	require.NoError(t, err)
+	// test node version
+	cmd := exec.Command(nodePath, "--version")
+	output, err := cmd.Output()
+	require.NoError(t, err)
+	r, err := regexp.Compile(`^v(\d+)`)
+	require.NoError(t, err)
+	matches := r.FindSubmatch(output)
+	require.Len(t, matches, 2)
+	version, err := strconv.Atoi(string(matches[1]))
+	if version < 16 {
+		t.Fatalf("node v16 or greater is required. Installed version: %s", output)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/xjslang/djs
 
 go 1.24
 
-require github.com/xjslang/xjs v0.9.1-alpha
+require (
+	github.com/stretchr/testify v1.11.1
+	github.com/xjslang/xjs v0.9.1-alpha
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
