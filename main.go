15eb42f23282ee00527afd405548fca0047afa80
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-06-17 12:26:13 +0100

    feat: add -stdin flag

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/xjslang/djs/djs"
@@ -11,23 +12,35 @@ import (
 )
 
 func main() {
-	var check, format bool
+	var check, format, stdin bool
 	flag.BoolVar(&format, "format", false, "format code")
 	flag.BoolVar(&check, "check", false, "check code")
+	flag.BoolVar(&stdin, "stdin", false, "read from stdin")
 	flag.Parse()
-
-	source := flag.Arg(0)
-	if flag.NArg() != 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <file.djs>\n", os.Args[0])
-		os.Exit(2)
-	}
 
 	if check && format {
 		fmt.Fprintf(os.Stderr, "-check and -format cannot be used together\n")
 		os.Exit(2)
 	}
 
-	input, err := os.ReadFile(source)
+	// read input from stdin or file
+	var input []byte
+	var err error
+	if n := flag.NArg(); stdin && n == 0 {
+		input, err = io.ReadAll(os.Stdin)
+	} else if n == 1 {
+		source := flag.Arg(0)
+		input, err = os.ReadFile(source)
+	} else {
+		cmd := os.Args[0]
+		fmt.Fprint(os.Stderr, "Usage:\n\n")
+		fmt.Fprintf(os.Stderr, "\t%s <file.djs>\n", cmd)
+		fmt.Fprintf(os.Stderr, "\t%s -format <file.djs>\n", cmd)
+		fmt.Fprintf(os.Stderr, "\t%s -check <file.djs>\n", cmd)
+		fmt.Fprintf(os.Stderr, "\t%s -check -stdin <<< \"DJS code\"\n", cmd)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(2)
+	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -49,7 +62,7 @@ func main() {
 	case check:
 		_, err := djs.Parse(input)
 		if errList, ok := err.(parser.ErrorList); err == nil || ok {
-			fmt.Fprintf(os.Stdout, "[")
+			fmt.Fprintf(os.Stdout, "{\"errors\": [\n")
 			for i, e := range errList {
 				var start, end token.Position
 				var msg, code string
@@ -61,16 +74,16 @@ func main() {
 					msg = e.Error()
 					code = "FATAL"
 				}
-				if i == 0 {
-					fmt.Fprint(os.Stdout, "\n")
+				if i > 0 {
+					fmt.Fprint(os.Stdout, ",\n")
 				}
-				fmt.Fprint(os.Stdout, "\t{range: {")
-				fmt.Fprintf(os.Stdout, "start: {line: %d, colum: %d}, ", start.Line, start.Column)
-				fmt.Fprintf(os.Stdout, "end: {line: %d, colum: %d}}}, ", end.Line, end.Column)
-				fmt.Fprintf(os.Stdout, "message: %q, ", msg)
-				fmt.Fprintf(os.Stdout, "code: %q}\n", code)
+				fmt.Fprint(os.Stdout, "\t{\"range\": {")
+				fmt.Fprintf(os.Stdout, "\"start\": {\"line\": %d, \"column\": %d}, ", start.Line, start.Column)
+				fmt.Fprintf(os.Stdout, "\"end\": {\"line\": %d, \"column\": %d}}, ", end.Line, end.Column)
+				fmt.Fprintf(os.Stdout, "\"message\": %q, ", msg)
+				fmt.Fprintf(os.Stdout, "\"code\": %q}", code)
 			}
-			fmt.Fprintf(os.Stdout, "]\n")
+			fmt.Fprintf(os.Stdout, "]}\n")
 		} else {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
