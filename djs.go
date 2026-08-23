35843c4abc6084f814b4648cb0bc381d745b1de3
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-08-23 11:39:43 +0100

    refactor: restructure

--- djs.go
+++ djs.go
@@ -1,6 +1,10 @@
 package djs
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+
 	"github.com/xjslang/xjs"
 	"github.com/xjslang/xjs/ast"
 	"github.com/xjslang/xjs/js"
@@ -9,6 +13,15 @@ import (
 	"github.com/xjslang/xjs/scanner"
 	"github.com/xjslang/xjs/token"
 )
+
+// DeferStmt represents a "defer" statement
+type DeferStmt struct {
+	ast.BaseStmt
+	Layout struct {
+		Defer token.Token
+	}
+	Stmt ast.Stmt
+}
 
 func Parse(input string) (*js.Program, error) {
 	deferTyp := token.RegisterType("DEFER", "defer")
@@ -47,7 +60,44 @@ func Parse(input string) (*js.Program, error) {
 
 func Compile(node ast.Node) (string, error) {
 	p := xjs.PrinterBuilder().
-		UsePrinter(compiler).
+		UsePrinter(func(p *printer.Printer, node ast.Node, next func(*printer.Printer, ast.Node) error) error {
+			switch v := node.(type) {
+			case *js.BlockStmt:
+				// tells the printer how to print blocks that contains "defer" statements
+				if isDeferBlock(v) {
+					defersName := fmt.Sprintf("__defers_%s__", rndID())
+
+					// share a context with its child nodes
+					ctx := p.PushContext()
+					defer p.PopContext()
+					ctx["defersName"] = defersName
+
+					p.Line().Print(fmt.Sprintf("{ let %s = []; try {", defersName))
+					if err := js.PrintBlockStmt(p, v); err != nil {
+						return err
+					}
+					p.Print(fmt.Sprintf("} finally {"+
+						"for (let defer of %s) { "+
+						"try { defer() } catch (e) { console.error(e) }"+
+						"}}}", defersName))
+					return nil
+				}
+			case *DeferStmt:
+				// tells the printer how to print "defer" statements
+				ctx := p.Context()
+				defersName, ok := ctx["defersName"]
+				if !ok {
+					// return printer.ErrorAt(v.Layout.Defer.Position, "defer cannot be used outside a block")
+					return p.Error("defer cannot be used outside a block")
+				}
+
+				p.
+					Line(). // ensure a new line is added before printing
+					Print(fmt.Sprintf("%s.unshift(() => {", defersName), v.Stmt, "});")
+				return nil
+			}
+			return next(p, node) // delegate in the next printer
+		}).
 		Build(printer.Compact())
 	p.Print(node)
 	return p.Output()
@@ -55,8 +105,35 @@ func Compile(node ast.Node) (string, error) {
 
 func Format(node ast.Node, opts ...printer.Option) (string, error) {
 	p := xjs.PrinterBuilder().
-		UsePrinter(formatter).
+		UsePrinter(func(pr *printer.Printer, node ast.Node, next func(*printer.Printer, ast.Node) error) error {
+			switch v := node.(type) {
+			case *DeferStmt:
+				pr.
+					Line(). // ensure a new line is added before printing
+					Print(v.Layout.Defer)
+				pr.
+					Space(). // ensure a new space is added before printing
+					Print(v.Stmt)
+				return nil
+			}
+			return next(pr, node)
+		}).
 		Build(opts...)
 	p.Print(node)
 	return p.Output()
+}
+
+func isDeferBlock(node *js.BlockStmt) bool {
+	for _, stmt := range node.Stmts {
+		if _, ok := stmt.(*DeferStmt); ok {
+			return true
+		}
+	}
+	return false
+}
+
+func rndID() string {
+	b := make([]byte, 4)
+	rand.Read(b) //nolint
+	return hex.EncodeToString(b)
 }
